9753f13eb37c610c7e172522bb99643b1c6bff4a
Charlie Tuna <ctunasson@gmail.com>
2023-02-05 14:36:21 +0100

    8

--- hereWeGoAgain.py
+++ hereWeGoAgain.py
@@ -1,14 +1,23 @@
-import socketcan
+# import socketcan
+# import can
+# import PyNMEA2
+
+# can_interface = 'can0'
+# bus = can.interface.Bus(can_interface, bustype='socketcan_native')
+
+# for msg in bus:
+#     if msg.arbitration_id == PyNMEA2.PRIVATE_ISOTP_ID:
+#         try:
+#             sentence = PyNMEA2.parse(msg.data)
+#             print(sentence)
+#         except PyNMEA2.ParseError as error:
+#             print("Parse error:", error)
+
+
 import can
-import PyNMEA2
 
-can_interface = 'can0'
-bus = can.interface.Bus(can_interface, bustype='socketcan_native')
+bus = can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
 
-for msg in bus:
-    if msg.arbitration_id == PyNMEA2.PRIVATE_ISOTP_ID:
-        try:
-            sentence = PyNMEA2.parse(msg.data)
-            print(sentence)
-        except PyNMEA2.ParseError as error:
-            print("Parse error:", error)
+while True:
+    msg = bus.recv()
+    print(f"ID: {msg.arbitration_id:x}, Data: {msg.data}")
