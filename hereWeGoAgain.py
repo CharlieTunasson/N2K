d17db8ca1a28485044386aca71fe84a9753c4a68
Charlie Tuna <ctunasson@gmail.com>
2023-02-08 15:28:16 +0100

    12

--- hereWeGoAgain.py
+++ hereWeGoAgain.py
@@ -1,3 +1,15 @@
+# import can
+
+# # Initialize the CAN bus using the "socketcan" interface
+# bus = can.Bus(channel='can0', bustype='socketcan')
+
+# # Continuously listen for incoming messages on the CAN bus
+# while True:
+#     message = bus.recv()
+#     if message is not None:
+#         # Print the message data
+#         print("ID: 0x{:03X} Data: {}".format(message.arbitration_id, message.data))
+
 import can
 
 # Initialize the CAN bus using the "socketcan" interface
--- nmea3.py
+++ nmea3.py
@@ -1,8 +1,20 @@
+# import can
+
+# # initialize the CAN bus
+# bus = can.interface.Bus(channel='can0', bustype='socketcan')
+
+# # listen to the incoming CAN messages
+# for msg in bus:
+#     print(msg)
+
 import can
 
-# initialize the CAN bus
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
+# Initialize the CAN bus using the "socketcan" interface
+bus = can.Bus(channel='can0', bustype='socketcan')
 
-# listen to the incoming CAN messages
-for msg in bus:
-    print(msg)
+# Continuously listen for incoming messages on the CAN bus
+while True:
+    message = bus.recv()
+    if message is not None:
+        # Print the message data
+        print("ID: 0x{:03X} Data: {}".format(message.arbitration_id, message.data))
